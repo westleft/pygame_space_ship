37d60cf9c25b739013189ec5acdaff0305a1a9a9
西左 <78782311+westleft@users.noreply.github.com>
2021-09-14 18:31:25 +0800

    add random rock

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import pygame
+import random
 
 FPS = 60
-WHITE = 255, 255, 255
-GREEN = 0, 255, 0
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
 
 WIDTH, HEIGHT = 500, 600
 
@@ -47,9 +49,38 @@ class Player(pygame.sprite.Sprite):
         elif self.rect.y < 0:
             self.rect.y = 0
 
+class Rock(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((30, 40))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(0, WIDTH - self.rect.width)
+        self.rect.y = random.randrange(-100, -40)
+        self.speedy = random.randrange(2, 10)
+        self.speedx = random.randrange(-3, 3)
+
+    def update(self):
+        self.rect.y += self.speedy
+        self.rect.x += self.speedx
+        if self.rect.top > HEIGHT or self.rect.left > WIDTH or self.rect.right <0:
+            self.rect.x = random.randrange(0, WIDTH - self.rect.width)
+            self.rect.y = random.randrange(-100, -40)
+            self.speedy = random.randrange(2, 10)
+            self.speedx = random.randrange(-3, 3)
+
+
 all_sprites = pygame.sprite.Group()
 player = Player()
 all_sprites.add(player)
+
+for i in range(8):
+    r = Rock()
+    all_sprites.add(r)
+
+rock = Rock()
+all_sprites.add(rock)
+
 
 while running:
     clock.tick(FPS)  # 一秒最多執行10次(FPS)
