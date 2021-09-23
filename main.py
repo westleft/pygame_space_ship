a8cca851bafd28f1db55262226db98a78cf8e624
西左 <78782311+westleft@users.noreply.github.com>
2021-09-23 17:25:14 +0800

    rock animation

--- main.py
+++ main.py
@@ -19,11 +19,25 @@ clock = pygame.time.Clock()
 # 載入圖片
 background_img = pygame.image.load(os.path.join("images","bg.jpg")).convert()
 player_img = pygame.image.load(os.path.join("img","player.png")).convert()
-rock_img = pygame.image.load(os.path.join("img","rock.png")).convert()
+# rock_img = pygame.image.load(os.path.join("img","rock.png")).convert()
 bullet_img = pygame.image.load(os.path.join("img","bullet.png")).convert()
+rock_imgs = []
+
+for i in range(7):
+    rock_imgs.append(pygame.image.load(os.path.join("img",f"rock{i}.png")).convert())
 
 pygame.display.set_caption("肥宅大冒險")
 running = True
+
+font_name = pygame.font.match_font('arial')
+
+def draw_text(surf, text, size, x, y):
+    font = pygame.font.Font(font_name, size)
+    text_surface = font.render(text, True, WHITE)
+    text_rect = text_surface.get_rect()
+    text_rect.centerx = x
+    text_rect.top = y
+    surf.blit(text_surface, text_rect)
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -68,16 +82,29 @@ class Player(pygame.sprite.Sprite):
 class Rock(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = rock_img
-        self.image.set_colorkey(BLACK)
+        self.image_ori = random.choice(rock_imgs)
+        self.image_ori.set_colorkey(BLACK)
+        self.image = self.image_ori.copy()
+        
         self.rect = self.image.get_rect()
-        self.radius = self.rect.width * 0.85 / 2
+        self.radius = int(self.rect.width * 0.85 / 2)
         self.rect.x = random.randrange(0, WIDTH - self.rect.width)
-        self.rect.y = random.randrange(-100, -40)
+        self.rect.y = random.randrange(-180, -100)
         self.speedy = random.randrange(2, 10)
         self.speedx = random.randrange(-3, 3)
+        self.total_degree = 0
+        self.rot_degree = random.randrange(-3, 3)
+
+    def rotate(self):
+        self.total_degree += self.rot_degree
+        self.total_degree = self.total_degree % 360
+        self.image = pygame.transform.rotate(self.image_ori, self.total_degree)
+        center = self.rect.center
+        self.rect = self.image.get_rect()
+        self.rect.center = center
 
     def update(self):
+        self.rotate()
         self.rect.y += self.speedy
         self.rect.x += self.speedx
         if self.rect.top > HEIGHT or self.rect.left > WIDTH or self.rect.right <0:
@@ -116,6 +143,7 @@ for i in range(8):
 rock = Rock()
 all_sprites.add(rock)
 
+score = 0
 
 while running:
     clock.tick(FPS)  # 一秒最多執行10次(FPS)
@@ -132,6 +160,7 @@ while running:
     all_sprites.update()
     hits = pygame.sprite.groupcollide(rocks, bullets, True, True)
     for hit in hits:
+        score += hit.radius
         r = Rock()
         all_sprites.add(r)
         rocks.add(r)
@@ -145,7 +174,9 @@ while running:
     screen.blit(background_img, (0, 0))
 
     all_sprites.draw(screen)
+    draw_text(screen, str(score), 18, WIDTH / 2, 10)
     pygame.display.update()
+
 
 
 pygame.quit()
